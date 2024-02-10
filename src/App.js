53934bcc716035528244346e2fdce88ac636dc09
aditya-varma8019 <rvenkataditya@gmail.com>
2024-02-10 14:03:30 +0530

    approval

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Login from './components/Auth/Login/Login'
 import Register from './components/Auth/Register/Register'
+import Approval from "./components/pages/Approval"
 function App() {
   return (
     <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path='/approval' element={<Approval />} />
         {/* <Route path' */}
       </Routes>
   );
