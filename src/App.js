be694273536bac5a711eb43ab2ca8e1614e5f38e
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-02-10 13:04:18 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
-        {/* <Route path' */}
+        {/* <Route path' */} //dd
       </Routes>
   );
 }
