046fbeace80bedb278dc65ad454540253c041cb4
aditya-varma8019 <rvenkataditya@gmail.com>
2024-02-10 19:33:35 +0530

    just before eval 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ApprovalRequestsTable from './components/pages/Approval';
 import Home from './components/Letter/Home';
 import Letter from './components/Letter/Letter';
 import StudentRequestsTable from './components/pages/StudentApproval';
-
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
@@ -15,13 +15,14 @@ function App() {
  
     <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/newevent" element={<Profile />} />
         <Route path='/approval' element={<ApprovalRequestsTable />} />
         <Route path="/letter" element={<Home/>} />
         <Route path='/generateletter' element={<Letter/>} />
         <Route path='/student' element={<StudentRequestsTable />} />
-
+        {/* <Route path='/' element={</>} /> */}
       </Routes>
     </>
   );
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../Auth/layout'
+
+const HomePage = () => {
+  return (
+    // <div>HomePage</div>
+    <Layout>
+    <h1>HomePage</h1>
+    </Layout>
+  )
+}
+
+export default HomePage
