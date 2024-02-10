00c676f123a99fea6b2a2ec8f571ac01b3d1e1f7
aditya-varma8019 <rvenkataditya@gmail.com>
2024-02-10 12:45:53 +0530

    start

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import BottomNavBar from "./BottomNavBar";
+
+const Layout = () => {
+  return (
+  <div>
+          <Header />  
+          
+          <BottomNavBar />  
+    </div>
+  );
+};
+
+export default Layout;
